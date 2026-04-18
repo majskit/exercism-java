f4a48500cfa2bc9ce84428019e0d0b1273c2682a
majskit <kmositdev@gmail.com>
2026-04-18 19:29:20 +0100

    add SalaryCalculator class -  ternary operator

---
+++ salary-calculator/SalaryCalculator.java
@@ -0,0 +1,20 @@
+package salary_calculator;
+
+public class SalaryCalculator {
+    public double salaryMultiplier(int daysSkipped) {
+        return daysSkipped >= 5 ? 0.85 : 1.0;
+    }
+
+    public int bonusMultiplier(int productsSold) {
+        return productsSold >= 20 ? 13 : 10;
+    }
+
+    public double bonusForProductsSold(int productsSold) {
+        return productsSold * bonusMultiplier(productsSold);
+    }
+
+    public double finalSalary(int daysSkipped, int productsSold) {
+        double result = 1000 * salaryMultiplier(daysSkipped) + bonusForProductsSold(productsSold);
+        return result > 2000.00 ? 2000.00 : result;
+    }
+}
