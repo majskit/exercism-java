a504717ee1d9b4b8f9091256296a1e90156ceb6a
majskit <kmositdev@gmail.com>
2026-04-29 15:45:02 +0100

    add FootballMatchReports class - switch statements

---
+++ football_match_reports/FootballMatchReports.java
@@ -0,0 +1,22 @@
+package football_match_reports;
+
+public class FootballMatchReports {
+    public static String onField(int shirtNum) {
+        return switch (shirtNum) {
+            case 1 -> "goalie";
+            case 2 -> "left back";
+            case 3, 4 -> "center back";
+            case 5 -> "right back";
+            case 6, 7, 8 -> "midfielder";
+            case 9 -> "left wing";
+            case 10 -> "striker";
+            case 11 -> "right wing";
+            default -> "invalid";
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FootballMatchReports.onField(10)); // striker
+        System.out.println(FootballMatchReports.onField(13)); // invalid
+    }
+}
